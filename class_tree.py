8ccb08b2cbffe6824ed65f04fef4e59c8b03ad37
tigransargsyan1994 <76061958+tigransargsyan1994@users.noreply.github.com>
2021-03-12 20:35:25 +0400

    'number_of_elements'

--- class_tree.py
+++ class_tree.py
@@ -12,24 +12,32 @@ def print_tree(node):
     print_tree(node.left)
     print_tree(node.right)
 
+
 def count_level(node):
     if node == None:
         return 0
 
     return max(count_level(node.left), count_level(node.right)) + 1
 
+
+def count_elements(node):
+    if node == None:
+        return 0
+
+    return count_elements(node.left) + count_elements(node.right) + 1
+
 root = Tree('root')
 root.left = Tree('left')
-root.right = Tree('right')
 root.left.left = Tree('left->left')
-root.left.left.left = Tree('left->left->left')
-root.left.left.right = Tree('left->left->right')
-root.left.left.right.right = Tree('left->left->right->right')
+root.right = Tree('right')
 root.left.right = Tree('left->right')
 
 
 print_tree(root)
 print('\n')
-level = count_level(root)
 
-print(level)
+level = count_level(root)
+print('Height of tree is', level)
+
+elements = count_elements(root)
+print('Number of elements is ', elements)
